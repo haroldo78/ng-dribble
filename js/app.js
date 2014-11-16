1e5028071e31162eeba7fc60d03d4864fd377751
Mandy Yeung <mandycodes@gmail.com>
2014-11-16 12:29:37 -0500

    Testing out controller properties

--- js/app.js
+++ js/app.js
@@ -2,5 +2,8 @@
   var app = angular.module('ngDribble', []);
   app.controller('AppCtrl', function(){
     this.name = "Mandy";
+    this.updateName = function(){
+      this.name = "World";
+    };
   });
 })();
