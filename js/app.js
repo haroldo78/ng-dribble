e7f202b4a3ca78f32f26f587d05b5aa60d09e38a
Mandy Yeung <mandycodes@gmail.com>
2014-11-16 17:52:31 -0500

    Add angular-route.js and load dependent module ngRoute

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 (function(){
-  var app = angular.module('ngDribble', ['ngDribble.controllers']);
+  var app = angular.module('ngDribble', ['ngRoute','ngDribble.controllers']);
+
+  app.config(function($routeProvider){
+    $routeProvider
+    .when('/:list',  {
+      controller: 'ShotsListsCtrl',
+      templateUrl: 'partials/shots_lists.html'
+    })
+    .otherwise({redirectTo: "/popular"});
+  });
 })();
--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,8 @@
 var controllers = angular.module('ngDribble.controllers', []);
 
-controllers.controller('ShotsListsCtrl', function($scope, $http){
-  $scope.list;
-  $http.jsonp('http://api.dribbble.com/shots/popular?callback=JSON_CALLBACK').then(function(data){
+controllers.controller('ShotsListsCtrl', function($scope, $routeParams, $http){
+  var list = $routeParams.list;
+  $http.jsonp('http://api.dribbble.com/shots/'+ list +'?callback=JSON_CALLBACK').then(function(data){
     $scope.list = data.data;
   });
-})
+});
