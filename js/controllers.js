560f0288c1eb5f6f52c27dfad61e7b2ce0a6b84c
Mandy Yeung <mandycodes@gmail.com>
2014-11-16 15:27:00 -0500

    Retrieve data from Dribble API with and display titles with ng-repeat

--- js/controllers.js
+++ js/controllers.js
@@ -1,11 +1,8 @@
 var controllers = angular.module('ngDribble.controllers', []);
 
-controllers.controller('AppCtrl', function(){
-});
-
-controllers.controller('ShotsListCtrl', function($http){
-  this.list;
+controllers.controller('ShotsListsCtrl', function($scope, $http){
+  $scope.list;
   $http.jsonp('http://api.dribbble.com/shots/popular?callback=JSON_CALLBACK').then(function(data){
-    console.log(data);
-  })
+    $scope.list = data.data;
+  });
 })
